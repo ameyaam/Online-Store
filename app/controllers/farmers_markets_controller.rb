2de2065c93f131c2087cda181112f50c1b8f0b01
Ameya More <ameyamore15@gmail.com>
2015-04-19 23:04:51 -0400

    adding backend for front page

--- app/controllers/farmers_markets_controller.rb
+++ app/controllers/farmers_markets_controller.rb
@@ -36,6 +36,29 @@ class FarmersMarketsController < ApplicationController
     respond_with(@farmers_market)
   end
 
+  def get_nearby_markets
+    response_hashlist = Hash.new
+    @nearby_markets = FarmersMarket.where("city = ?", params[:city])
+    puts"**************PRINTING FARMERS MARKETS**************"
+    
+    i = 0
+    @nearby_markets.each do |market|
+      response_hashlist[i] = market.as_json
+      i = i + 1
+    end
+
+    puts response_hashlist
+
+    puts response_hashlist.to_json
+    #respond_with(response_hashlist)
+
+    respond_to do |format|
+      #msg = {:name => "ameya", :city => "philly"}
+      msg = response_hashlist
+      format.json {render :json => msg}
+    end
+  end
+  
   private
     def set_farmers_market
       @farmers_market = FarmersMarket.find(params[:id])
