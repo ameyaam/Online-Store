12069af7f5d98e67471e2ddefe6a39b36678c15c
Shivam Dalal <shivamda@seas.upenn.edu>
2015-05-07 23:41:41 -0400

    Secret token commented

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Project::Application.config.secret_token = '111813f4fc7b5857ff002393ce9deb8bf293eaeac0de4f2582758ba1b4a25591facf42347240fc208f07c3b55cf7369534a34c250098b450edd2528f879378b3'
+#Project::Application.config.secret_token = '111813f4fc7b5857ff002393ce9deb8bf293eaeac0de4f2582758ba1b4a25591facf42347240fc208f07c3b55cf7369534a34c250098b450edd2528f879378b3'
